86864c0aa12a5bdec11e9180ee27eed16306161a
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-01 17:53:00 +0300

    Update index.js
    add ThemeProvider, BrowserRouter, GlobalStyles and remove file index.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { theme, GlobalStyles } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/event-planner">
+        <GlobalStyles />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
