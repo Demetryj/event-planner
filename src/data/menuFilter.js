d6998d81682496d4f774d835fdbccbd30dc706b7
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-03 23:45:20 +0300

    Create menuFilter.js

---
+++ src/data/menuFilter.js
@@ -0,0 +1,14 @@
+// import { BsArrowUpShort } from 'react-icons/bs';
+// import { BsArrowDownShort } from 'react-icons/bs';
+
+export const category = [
+  'Art',
+  'Music',
+  'Business',
+  'Conference',
+  'Workshop',
+  'Party',
+  'Sport',
+];
+
+export const sortBy = ['by name', 'by data', 'by priority'];
