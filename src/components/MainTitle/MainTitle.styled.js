5c341b5cf581844165f6531b22f8f43095afd475
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-03 23:33:24 +0300

    Create MainTitle.styled.js

---
+++ src/components/MainTitle/MainTitle.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+  margin-bottom: ${p => p.theme.spacing(6)};
+
+  font-size: ${p => p.theme.fontSizes.l};
+  font-weight: ${p => p.theme.fontWeights.semiBold};
+
+  color: ${p => p.theme.colors.mainTitle};
+
+  @media screen and (${p => p.theme.media.medium}) {
+    font-size: ${p => p.theme.fontSizes.xl};
+  }
+
+  @media screen and (${p => p.theme.media.large}) {
+    margin-bottom: ${p => p.theme.spacing(2.4)};
+  }
+`;
