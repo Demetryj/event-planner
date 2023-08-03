e95641dea87734d19ac16d133377cdd4ece6b0ba
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-03 23:32:59 +0300

    Create ArrowBack.jsx

---
+++ src/components/ArrowBack/ArrowBack.jsx
@@ -0,0 +1,13 @@
+import { useLocation } from 'react-router-dom';
+import { Wrapper, Text } from './ArrowBack.styled';
+import { GoArrowLeft } from 'react-icons/go';
+
+export const ArrowBack = () => {
+  const location = useLocation();
+  return (
+    <Wrapper to={location.state.from}>
+      <GoArrowLeft />
+      <Text>Back</Text>
+    </Wrapper>
+  );
+};
