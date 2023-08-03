d756425fc2312f8f448fba7f3fb7a403ecba2ced
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-03 23:59:06 +0300

    add rrovider redux-store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
+import { store } from 'redux/store';
 import { theme, GlobalStyles } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/event-planner">
-        <GlobalStyles />
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter basename="/event-planner">
+          <GlobalStyles />
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
