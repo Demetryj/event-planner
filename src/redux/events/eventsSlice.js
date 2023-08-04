90a02853b67659702694403a9c8cbc857dc606b3
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-05 00:08:52 +0300

    add handling of deleting an event by the reducer

--- src/redux/events/eventsSlice.js
+++ src/redux/events/eventsSlice.js
@@ -3,13 +3,17 @@ import { events } from 'data/dataEvents';
 
 export const EventsSlice = createSlice({
   name: 'events',
-  initialState: events,
+  initialState: [...events],
   reducers: {
     addEvent(state, action) {
       return;
     },
+    deleteEvent(state, action) {
+      const index = state.findIndex(oneEvent => oneEvent.id === action.payload);
+      state.splice(index, 1);
+    },
   },
 });
 
-export const { addEvent } = EventsSlice.actions;
+export const { addEvent, deleteEvent } = EventsSlice.actions;
 export const eventsReducer = EventsSlice.reducer;
