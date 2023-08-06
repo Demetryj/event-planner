1fa03c7b1fb9563d63020a800fdf5c9fb5de0cca
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-07 00:02:48 +0300

    add title snd filterBar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
+import { Box } from 'components/Box';
 import { FilterBar } from 'components/FilterBar';
+import { MainTitle } from 'components/MainTitle';
 import { CardList } from 'components/CardList';
 
 const Home = () => {
   return (
     <>
-      <FilterBar />
+      <Box
+        display={{ lg: 'flex' }}
+        flexDirection={{ lg: 'row-reverse' }}
+        alignItems={{ lg: 'center' }}
+        justifyContent={{ lg: 'space-between' }}
+        mb={{ lg: 40 }}
+      >
+        <FilterBar />
+        <MainTitle display="desktop">My events</MainTitle>
+      </Box>
+
       <CardList />
     </>
   );
