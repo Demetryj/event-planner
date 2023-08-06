e1fac48bc0e6c0946b1685b5807f3fff3f993101
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-07 00:02:26 +0300

    add import components

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -1,5 +1,15 @@
+import { ArrowBack } from 'components/ArrowBack';
+import { MainTitle } from 'components/MainTitle';
+import { EditForm } from 'components/EditForm';
+
 const CreateEvent = () => {
-  return <div>CreateEvent</div>;
+  return (
+    <>
+      <ArrowBack />
+      <MainTitle>Create new event</MainTitle>
+      <EditForm />
+    </>
+  );
 };
 
 export default CreateEvent;
