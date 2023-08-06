c185803a514795328f51d692d6aa1af3d4ce631b
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-06 23:46:48 +0300

    Create SelectMenu.styled.js

---
+++ src/components/SelectMenu/SelectMenu.styled.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const MenuList = styled.ul`
+  position: absolute;
+  top: ${p => p.theme.spacing(25)};
+  padding-left: ${p => p.theme.spacing(4)};
+  padding-right: ${p => p.theme.spacing(4)};
+  width: 100%;
+  z-index: 10;
+
+  background-color: ${p => p.theme.colors.backgroundWhite};
+  border-radius: ${p => p.theme.radii.s};
+  box-shadow: ${p => p.theme.shadows.main};
+`;
+
+export const MenuItem = styled.li`
+  padding-top: ${p => p.theme.spacing(4)};
+  padding-bottom: ${p => p.theme.spacing(4)};
+
+  &:not(:last-child) {
+    border-bottom: ${p => p.theme.borders.normal};
+    border-color: ${p => p.theme.colors.borderMenu};
+  }
+
+  cursor: pointer;
+
+  & p {
+    color: ${p => p.theme.colors.bar};
+    transition: ${p => p.theme.transitions.main};
+  }
+
+  &:hover p,
+  &:focus p {
+    color: ${p => p.theme.colors.purpleText};
+  }
+`;
+
+export const Text = styled.p`
+  font-size: ${p => p.theme.fontSizes.m};
+  font-weight: ${p => p.theme.fontWeights.regular};
+`;
