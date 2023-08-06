8430b00c98e6c7ad1e6fb1dc8c9115edb919326b
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-06 23:54:14 +0300

    add event deletion and redirect to the main page after deleting the event card

--- src/components/EventInfo/EventInfo.jsx
+++ src/components/EventInfo/EventInfo.jsx
@@ -1,6 +1,8 @@
-import { useSelector } from 'react-redux';
-import { Link, useLocation, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useLocation, useParams, useNavigate } from 'react-router-dom';
+
 import { selectEvents } from 'redux/events/selectors';
+import { deleteEvent } from 'redux/events/eventsSlice';
 import { getEventById } from 'utils/getEventById';
 import {
   Wrapper,
@@ -19,6 +21,8 @@ export const EventInfo = () => {
   const { id } = useParams();
   const events = useSelector(selectEvents);
   const location = useLocation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const data = getEventById(events, id);
   const {
@@ -30,6 +34,11 @@ export const EventInfo = () => {
     priority,
     picture,
   } = data;
+
+  const handleDeleteEvent = () => {
+    dispatch(deleteEvent(id));
+    navigate('/', { replace: true });
+  };
 
   return (
     <Wrapper>
@@ -49,7 +58,9 @@ export const EventInfo = () => {
             <BtnEdit type="button">Edit</BtnEdit>
           </Link>
 
-          <BtnDelete type="button">Delete event</BtnDelete>
+          <BtnDelete type="button" onClick={handleDeleteEvent}>
+            Delete event
+          </BtnDelete>
         </WrapperButton>
       </WrapperInfo>
     </Wrapper>
