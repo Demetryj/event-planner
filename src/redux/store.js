1bdad9926bf0b0dcbbf158c4712a8004d4c57a04
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-07 12:23:52 +0300

    add state filter

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { eventsReducer } from './events/eventsSlice';
+import { filterReducer } from './filter/filterSlice';
 
 export const store = configureStore({
   reducer: {
     events: eventsReducer,
+    filter: filterReducer,
   },
 });
