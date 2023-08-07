8f6ad492ff332488485160286389fd6cf582c833
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-07 22:09:25 +0300

    Update FilterBar.jsx
    add processing of the menu closing/opening event and add editing of the selected filter value to the reducer

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 import { HiOutlinePlus } from 'react-icons/hi';
 import { CiFilter } from 'react-icons/ci';
@@ -7,40 +8,70 @@ import { Sort } from 'components/Sort';
 import { FilterMenu } from 'components/FilterMenu/FilterMenu';
 import { category, sortBy } from 'data/menuFilter';
 import { useMedia } from 'hooks/useMedia';
-import { WrapperBar, Text, BtnAdd, TextBtn } from './FilterBar.styled';
+import { chooseCategory, sortByValue } from 'redux/filter/filterSlice';
+import { selectCategory } from 'redux/filter/selectors';
+import { WrapperBar, BtnAdd, TextBtn } from './FilterBar.styled';
 
 export const FilterBar = () => {
   const [isOpenSort, setIsOpenSort] = useState(false);
   const [isOpenCategory, setIsOpenCategory] = useState(false);
   const { isMobile } = useMedia();
   const location = useLocation();
+  const dispach = useDispatch();
+  const selectedСategory = useSelector(selectCategory);
 
-  const handleClickSort = () => setIsOpenSort(!isOpenSort);
-  const handleClickCategory = () => setIsOpenCategory(!isOpenCategory);
+  const handleClickSort = () => {
+    setIsOpenCategory(false);
+    setIsOpenSort(!isOpenSort);
+  };
+
+  const handleClickCategory = () => {
+    setIsOpenSort(false);
+    setIsOpenCategory(!isOpenCategory);
+  };
+
+  const handleChooseCategory = e => {
+    dispach(chooseCategory(e.currentTarget.textContent));
+    setIsOpenCategory(false);
+  };
+
+  const handleChooseSortBy = e => {
+    dispach(sortByValue(e.currentTarget.dataset.action));
+    setIsOpenSort(false);
+  };
 
   return (
     <WrapperBar isOpenSort={isOpenSort}>
       {!isOpenCategory ? (
         <Sort onClick={handleClickCategory}>
-          <Text>Category</Text>
+          <p>{selectedСategory ? selectedСategory : 'Category'}</p>
           <CiFilter />
         </Sort>
       ) : (
-        <FilterMenu array={category}>
-          <p>Category</p>
+        <FilterMenu
+          array={category}
+          onClick={handleClickCategory}
+          onChooseCategory={handleChooseCategory}
+        >
+          <p>{selectedСategory ? selectedСategory : 'Category'}</p>
           <CiFilter />
         </FilterMenu>
       )}
 
       {!isOpenSort ? (
         <Sort onClick={handleClickSort}>
-          <Text>Sort By</Text>
+          <p>Sort By</p>
           <LiaSlidersHSolid />
         </Sort>
       ) : (
-        <FilterMenu array={sortBy} variant="sortBy">
+        <FilterMenu
+          array={sortBy}
+          variant="sortBy"
+          onClick={handleClickSort}
+          chooseSortBy={handleChooseSortBy}
+        >
           {isMobile && <LiaSlidersHSolid />}
-          <p>Sort by</p>
+          <p>Sort By</p>
           {!isMobile && <LiaSlidersHSolid />}
         </FilterMenu>
       )}
