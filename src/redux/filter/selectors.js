307e6cade642ac8d9b5d67e0d22c0de3c5332e73
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-07 22:12:38 +0300

    Update selectors.js
    add event filtering by keyword, category and merging

--- src/redux/filter/selectors.js
+++ src/redux/filter/selectors.js
@@ -1,17 +1,32 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { selectEvents } from 'redux/events/selectors';
+import { sortEventsByValue } from 'utils/sortEventsByValue';
 export const selectSearch = state => state.filter.search;
+export const selectCategory = state => state.filter.category;
+export const selectSortBy = state => state.filter.sortBy;
 
 export const selectVisibleEvents = createSelector(
-  [selectEvents, selectSearch],
-  (events, search) => {
+  [selectEvents, selectSearch, selectCategory, selectSortBy],
+  (events, search, categoryValue, sortBy) => {
     const normalyzeFilter = search.toLowerCase();
 
-    const visibleEvents = events.filter(
-      event =>
-        event.title.toLowerCase().includes(normalyzeFilter) ||
-        event.description.toLowerCase().includes(normalyzeFilter)
-    );
+    let visibleEvents = events;
+
+    if (categoryValue) {
+      visibleEvents = events.filter(event => event.category === categoryValue);
+    }
+
+    if (search) {
+      visibleEvents = events.filter(
+        event =>
+          event.title.toLowerCase().includes(normalyzeFilter) ||
+          event.description.toLowerCase().includes(normalyzeFilter)
+      );
+    }
+
+    if (sortBy) {
+      visibleEvents = sortEventsByValue(events, sortBy);
+    }
 
     return visibleEvents;
   }
