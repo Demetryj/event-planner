33003f00c0302d312df5c1aa4b4a54eea4a1c320
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-07 22:13:48 +0300

    Update filterSlice.js
    add a sorting reducer by category and sorting

--- src/redux/filter/filterSlice.js
+++ src/redux/filter/filterSlice.js
@@ -2,13 +2,20 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const filterSlice = createSlice({
   name: 'filter',
-  initialState: { search: '' },
+  initialState: { search: '', category: '', sortBy: '' },
   reducers: {
     changeFilter(state, action) {
       state.search = action.payload;
     },
+    chooseCategory(state, action) {
+      state.category = action.payload;
+    },
+    sortByValue(state, action) {
+      state.sortBy = action.payload;
+    },
   },
 });
 
-export const { changeFilter } = filterSlice.actions;
+export const { changeFilter, chooseCategory, sortByValue } =
+  filterSlice.actions;
 export const filterReducer = filterSlice.reducer;
