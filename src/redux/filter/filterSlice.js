4bf7fd0be86d04c326e5826e225ec8a3c50139e4
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-07 12:18:33 +0300

    Create filterSlice.js

---
+++ src/redux/filter/filterSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const filterSlice = createSlice({
+  name: 'filter',
+  initialState: { search: '' },
+  reducers: {
+    changeFilter(state, action) {
+      state.search = action.payload;
+    },
+  },
+});
+
+export const { changeFilter } = filterSlice.actions;
+export const filterReducer = filterSlice.reducer;
