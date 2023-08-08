feaafc5a2a6deb82347eed9fbb20eb2c096d0125
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-08 15:38:24 +0300

    Update SharedLayout.jsx
    add location

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,17 @@
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { AppBar } from 'components/AppBar';
 import { Section, Container } from './SharedLayout.syled';
 
 export const SharedLayout = () => {
+  const location = useLocation();
+
   return (
     <>
       <AppBar />
       <main>
         <Section>
-          <Container>
+          <Container location={location.pathname}>
             <Suspense fallback={null}>
               <Outlet />
             </Suspense>
