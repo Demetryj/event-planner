3e8bcaa2ad47ac96cedf4725a4ae0d433377d167
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-08 15:37:59 +0300

    Update EditEvent.jsx
    add components

--- src/pages/EditEvent.jsx
+++ src/pages/EditEvent.jsx
@@ -1,5 +1,39 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { ArrowBack } from 'components/ArrowBack';
+import { MainTitle } from 'components/MainTitle';
+import { EditForm } from 'components/EventForm';
+import { editSchema } from 'data/editSchema';
+import { selectEvents } from 'redux/events/selectors';
+import { getEventById } from 'utils/getEventById';
+
 const EditEvent = () => {
-  return <div>Edit Event</div>;
+  const { id } = useParams();
+  const events = useSelector(selectEvents);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const event = getEventById(events, id);
+  const updateEvent = () => {
+    navigate(`/event/${id}`);
+  };
+
+  const initialValues = { ...event, picture: '' };
+
+  return (
+    <>
+      <ArrowBack />
+      <MainTitle>Edit event</MainTitle>
+      <EditForm
+        initialValues={initialValues}
+        editSchema={editSchema}
+        typeForm="Save"
+        location={location}
+        onButtonClick={updateEvent}
+      />
+    </>
+  );
 };
 
 export default EditEvent;
