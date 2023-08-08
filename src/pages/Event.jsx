0304ae2b78fbe58b0266fb1fe8c4e74ddb7a6b2c
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-08 15:38:14 +0300

    add location

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,15 +1,18 @@
+import { useLocation } from 'react-router-dom';
 import { Box } from 'components/Box';
 import { ArrowBack } from 'components/ArrowBack';
 import { MainTitle } from 'components/MainTitle/';
 import { EventInfo } from 'components/EventInfo';
 
 const Event = () => {
+  const location = useLocation();
+
   return (
     <>
       <ArrowBack />
       <Box mx="auto" maxWidth={{ lg: 628 }}>
         <MainTitle>Galery Opening</MainTitle>
-        <EventInfo />
+        <EventInfo location={location} />
       </Box>
     </>
   );
