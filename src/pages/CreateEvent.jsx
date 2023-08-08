34a0fe15ae503f9978529e7bb01230651962f700
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-08-08 15:37:46 +0300

    Update CreateEvent.jsx
    add components

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -1,13 +1,44 @@
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { nanoid } from 'nanoid';
 import { ArrowBack } from 'components/ArrowBack';
 import { MainTitle } from 'components/MainTitle';
-import { EditForm } from 'components/EditForm';
+import { EditForm } from 'components/EventForm';
+import { addEvent } from 'redux/events/eventsSlice';
+import { editSchema } from 'data/editSchema';
+import imgDefault from 'images/default-H@2x.jpg';
+
+const initialValues = {
+  title: '',
+  description: '',
+  date: '',
+  time: '',
+  location: '',
+  category: '',
+  picture: '',
+  priority: '',
+};
 
 const CreateEvent = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const createNewEvent = values => {
+    const newEvent = { ...values, picture: imgDefault, id: nanoid() };
+    dispatch(addEvent(newEvent));
+  };
+
   return (
     <>
       <ArrowBack />
       <MainTitle>Create new event</MainTitle>
-      <EditForm />
+      <EditForm
+        initialValues={initialValues}
+        editSchema={editSchema}
+        typeForm="Add event"
+        location={location}
+        onButtonClick={createNewEvent}
+      />
     </>
   );
 };
