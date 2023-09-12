bccc0da0455bf5ddd327b21812df440fef7e4d95
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-12 19:46:47 +0300

    Create PaginationCount.jsx

---
+++ src/components/PaginationCount/PaginationCount.jsx
@@ -0,0 +1,12 @@
+import { PaginationElement } from './PaginationCount.styled';
+
+export const PaginationCount = () => {
+  return (
+    <PaginationElement
+      count={988}
+      siblingCount={-1}
+      // page={page}
+      // onChange={handleChange}
+    />
+  );
+};
