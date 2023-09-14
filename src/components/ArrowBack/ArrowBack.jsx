023fdffa0ccf9fb9f98a969b4402e39cf83c58d1
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-14 23:09:52 +0300

    add translation arrow back

--- src/components/ArrowBack/ArrowBack.jsx
+++ src/components/ArrowBack/ArrowBack.jsx
@@ -1,14 +1,16 @@
 import { useLocation } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import { Wrapper, Text } from './ArrowBack.styled';
 import { GoArrowLeft } from 'react-icons/go';
 
 export const ArrowBack = () => {
   const location = useLocation();
+  const { t } = useTranslation();
 
   return (
     <Wrapper to={location.state?.from ?? '/'}>
       <GoArrowLeft />
-      <Text>Back</Text>
+      <Text>{t('arrowBack')}</Text>
     </Wrapper>
   );
 };
