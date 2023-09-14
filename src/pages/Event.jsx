37c24f1b1ad5de25f62fe18506fd862795cf8364
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-14 23:21:35 +0300

    change title

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,18 +1,25 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectEvents } from 'redux/events/selectors';
 import { Box } from 'components/Box';
 import { ArrowBack } from 'components/ArrowBack';
 import { MainTitle } from 'components/MainTitle/';
 import { EventInfo } from 'components/EventInfo';
+import { getEventById } from 'utils/getEventById';
 
 const Event = () => {
+  const { id } = useParams();
+  const events = useSelector(selectEvents);
   const location = useLocation();
+
+  const eventData = getEventById(events, id);
 
   return (
     <>
       <ArrowBack />
       <Box mx="auto" maxWidth={{ lg: 628 }}>
-        <MainTitle>Galery Opening</MainTitle>
-        <EventInfo location={location} />
+        <MainTitle>{eventData.title}</MainTitle>
+        <EventInfo location={location} data={eventData} />
       </Box>
     </>
   );
