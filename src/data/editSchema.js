bb68dfa848cac9dbbd6670c940d6ab021e9aeb47
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-15 19:37:17 +0300

    change the regular expression to allow the use of apostrophes and Ukrainian letters

--- src/data/editSchema.js
+++ src/data/editSchema.js
@@ -1,11 +1,18 @@
 import { object, string } from 'yup';
+import i18n from 'i18next';
 
 export const editSchema = object({
-  title: string().matches(/^[a-zA-Zа-яА-Я ]+$/, 'Invalid input'),
+  title: string().matches(
+    /^[a-zA-Z\u0400-\u04FF' ]+$/,
+    i18n.t('eventForm.errorMessage')
+  ),
   description: string(),
   date: string(),
   time: string(),
-  location: string().matches(/^[a-zA-Zа-яА-Я ]+$/, 'Invalid input'),
+  location: string().matches(
+    /^[a-zA-Z\u0400-\u04FF' ]+$/,
+    i18n.t('eventForm.errorMessage')
+  ),
   category: string(),
   picture: string(),
   priority: string(),
