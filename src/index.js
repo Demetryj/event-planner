33b42c9df7567d156d3e4fd27e8167e9af0c9ccb
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-09-24 21:56:48 +0300

    add PersistGate, persistor

--- src/index.js
+++ src/index.js
@@ -2,23 +2,26 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { App } from 'components/App';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider } from 'styled-components';
-import { store } from 'redux/store';
+import { persistor, store } from 'redux/store';
+import { App } from 'components/App';
 import { theme, GlobalStyles } from 'styles';
 import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter basename="/event-planner">
-          <GlobalStyles />
-          <Suspense fallback={null}>
-            <App />
-          </Suspense>
-        </BrowserRouter>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter basename="/event-planner">
+            <GlobalStyles />
+            <Suspense fallback={null}>
+              <App />
+            </Suspense>
+          </BrowserRouter>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
