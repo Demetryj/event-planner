74b93e00e82c95ec1c3bbeeab7b41105c0993de1
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-10-12 17:14:49 +0300

    add method required

--- src/data/editSchema.js
+++ src/data/editSchema.js
@@ -2,18 +2,22 @@ import { object, string } from 'yup';
 import i18n from 'i18next';
 
 export const editSchema = object({
-  title: string().matches(
-    /^[a-zA-Z\u0400-\u04FF' ]+$/,
-    i18n.t('eventForm.errorMessage')
-  ),
-  description: string(),
-  date: string(),
-  time: string(),
-  location: string().matches(
-    /^[a-zA-Z\u0400-\u04FF' ]+$/,
-    i18n.t('eventForm.errorMessage')
-  ),
-  category: string(),
+  title: string()
+    .matches(
+      /^[a-zA-Z\u0400-\u04FF' ]+$/,
+      i18n.t('eventForm.errorMessageInvalid')
+    )
+    .required(i18n.t('eventForm.errorMessageRequired')),
+  description: string().required(i18n.t('eventForm.errorMessageRequired')),
+  date: string().required(i18n.t('eventForm.errorMessageRequired')),
+  time: string().required(i18n.t('eventForm.errorMessageRequired')),
+  location: string()
+    .matches(
+      /^[a-zA-Z\u0400-\u04FF' ]+$/,
+      i18n.t('eventForm.errorMessageInvalid')
+    )
+    .required(i18n.t('eventForm.errorMessageRequired')),
+  category: string().required(i18n.t('eventForm.errorMessageRequired')),
   picture: string(),
-  priority: string(),
+  priority: string().required(i18n.t('eventForm.errorMessageRequired')),
 });
