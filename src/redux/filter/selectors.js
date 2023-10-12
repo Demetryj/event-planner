3db217a83f85089e6cc08f7d93c8f9485eb0a7fc
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-10-12 17:15:08 +0300

    add translate for categoryValue

--- src/redux/filter/selectors.js
+++ src/redux/filter/selectors.js
@@ -2,6 +2,7 @@ import { createSelector } from '@reduxjs/toolkit';
 import { selectEvents } from 'redux/events/selectors';
 import { sortEventsByValue } from 'utils/sortEventsByValue';
 import { translateData } from 'utils/translateData';
+
 export const selectSearch = state => state.filter.search;
 export const selectCategory = state => state.filter.category;
 export const selectSortBy = state => state.filter.sortBy;
@@ -15,7 +16,7 @@ export const selectVisibleEvents = createSelector(
 
     if (categoryValue) {
       return events.filter(
-        event => translateData(event.category) === categoryValue
+        event => translateData(event.category) === translateData(categoryValue)
       );
     }
 
